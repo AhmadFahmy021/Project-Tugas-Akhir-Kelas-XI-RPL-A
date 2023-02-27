31d674a02f781a56a01d93985631b7a75cc8ca22
AhmadFahmy021 <ahmadfahmy219@gmail.com>
2023-02-27 09:26:26 +0700

    Update and fix Bug

--- admin/invoice_bayar.php
+++ admin/invoice_bayar.php
@@ -2,8 +2,8 @@
 include '../functions.php';
 $id = $_GET['id'];
 // $sql = "SELECT * FROM pembayaran p Join kamar k ON k.idkamar=p.idkamar WHERE p.idpembayaran = $id";
-$sql = "SELECT * FROM penghuni p INNER JOIN kamar k on p.idkamar=k.idkamar INNER JOIN status s ON p.idstatus=s.idstatus WHERE p.idpenghuni = $id";
-// $sql = "SELECT * FROM pembayaran p INNER Join kamar k ON k.idkamar=p.idkamar INNER JOIN status s ON s.idstatus=p.idstatus WHERE p.idpembayaran = $id";
+// $sql = "SELECT * FROM penghuni p INNER JOIN kamar k on p.idkamar=k.idkamar INNER JOIN status s ON p.idstatus=s.idstatus WHERE p.idpenghuni = $id";
+$sql = "SELECT * FROM pembayaran p INNER Join kamar k ON k.idkamar=p.idkamar INNER JOIN status s ON s.idstatus=p.idstatus WHERE p.idpembayaran = $id";
 $r = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($r);
 require_once __DIR__ . '/vendor/autoload.php';
